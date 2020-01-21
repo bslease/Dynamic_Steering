ac82ad5c0c11041d21a59341d42b725819f52fb2
bslease <bslease@gmail.com>
2020-01-21 06:07:30 -0700

    Initial Commit
    demonstrating seek, flee, arrive, face, align, and lookwheregoing

---
+++ Dynamic Steering Basics/Assets/Face.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : Align
+{
+    // override Align's getTargetAngle to face the target instead of matching it's orientation
+    public override float getTargetAngle()
+    {
+        // work out the direction to the target
+        Vector3 direction = target.transform.position - character.transform.position;
+
+        // TODO - add a check in case we're right on top of the target
+
+        // derive the facing angle from the direction from me to target
+        float targetAngle = Mathf.Atan2(direction.x, direction.z); // radians
+        targetAngle *= Mathf.Rad2Deg;
+
+        return targetAngle;
+    }
+}
---
+++ Dynamic Steering Basics/Assets/Seek.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek
+{
+    public Kinematic character;
+    public GameObject target;
+
+    float maxAcceleration = 100f;
+
+    public bool flee = false;
+
+    public SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+
+        // Get the direction to the target
+        if (flee)
+        {
+            result.linear = character.transform.position - target.transform.position;
+        }
+        else
+        {
+            result.linear = target.transform.position - character.transform.position;
+        }
+
+        // give full acceleration along this direction
+        result.linear.Normalize();
+        result.linear *= maxAcceleration;
+
+        result.angular = 0;
+        return result;
+    }
+}
