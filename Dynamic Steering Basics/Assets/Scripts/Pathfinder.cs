f4c048d6a11f014f8d585a0fb05497b13ddae235
bslease <bslease@gmail.com>
2020-02-25 15:19:09 -0700

    demonstrate pathfidning

--- Dynamic Steering Basics/Assets/Scripts/Pathfinder.cs
+++ Dynamic Steering Basics/Assets/Scripts/Pathfinder.cs
@@ -2,28 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pathfinder : MonoBehaviour
+public class Pathfinder : Kinematic
 {
     public Node start;
     public Node goal;
     Graph myGraph;
 
+    FollowPath myMoveType;
+    LookWhereGoing myRotateType;
+
+    //public GameObject[] myPath = new GameObject[4];
+    GameObject[] myPath;
+
     // Start is called before the first frame update
     void Start()
     {
+        myRotateType = new LookWhereGoing();
+        myRotateType.character = this;
+        myRotateType.target = myTarget;
+
         Graph myGraph = new Graph();
         myGraph.Build();
         List<Connection> path = Dijkstra.pathfind(myGraph, start, goal);
+        // path is a list of connections - convert this to gameobjects for the FollowPath steering behavior
+        myPath = new GameObject[path.Count + 1];
+        int i = 0;
         foreach (Connection c in path)
         {
             Debug.Log("from " + c.getFromNode() + " to " + c.getToNode() + " @" + c.getCost());
+            myPath[i] = c.getFromNode().gameObject;
+            i++;
         }
+        myPath[i] = goal.gameObject;
+
+        myMoveType = new FollowPath();
+        myMoveType.character = this;
+        myMoveType.path = myPath;
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-
-        
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.angular = myRotateType.getSteering().angular;
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        base.Update();
     }
+
 }
